fddc603fc64295662fe346ea36aadb81c7fa2317
Matt Way <git@mway.me>
2013-11-20 09:53:31 -0500

    test for symlink before unlinking

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for f in .*; do
     if [[ ! $f =~ ^\.(git|\.)?$ ]]; then
-        unlink "$HOME/$f"
+        [ -h "$HOME/$f" ] && unlink "$HOME/$f"
         ln -s "$DIR/$f" "$HOME/$f"
     fi
 done
