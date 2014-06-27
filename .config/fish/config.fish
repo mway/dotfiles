21b2c75755b5100e629bc59bfa4a98941ec1c0ea
Matt Way <io@mway.co>
2014-06-27 11:28:48 -0400

    include local ruby/golang envs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 if status --is-login
     # Set paths
-    set PATH /usr/local/bin /usr/local/sbin /opt/local/bin /opt/local/sbin $PATH
+    set PATH /usr/local/bin /usr/local/sbin /opt/local/bin /opt/local/sbin /usr/local/opt/ruby/bin  $PATH
 end
 
 set DOCKER_HOST tcp://localhost:4243
-
+set GOPATH /usr/local/Cellar/go/lib
