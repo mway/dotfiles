02af27390c100cf491dbb16617e255780bfcbbf5
Matt Way <mway@uber.com>
2017-06-24 20:52:38 -0400

    Add simple installer

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+_install_dotfiles() {
+  local DIR="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" && pwd)"
+  local files=$(\ls -a "${DIR}" | grep '^\.' | grep -vE '^(\.|\.\.|\.git)$')
+
+  if [ "$1" == "force" ]; then
+    for f in $files; do
+      if [ -d "${HOME}/${f}" ]; then
+        rm -r "${HOME}/${f}"
+      elif [ -e "${HOME}/${f}" ]; then
+        unlink "${HOME}/${f}"
+      fi
+    done
+  fi
+
+  for f in $files; do
+    if [ ! -e "${HOME}/${f}" ]; then
+      ln -s "${DIR}/${f}" "${HOME}/${f}"
+      echo "Created ${HOME}/${f}."
+    elif [ -s "${HOME}/${f}" ]; then
+      echo "${HOME}/${f} is already a symlink."
+    else
+      echo "${HOME}/${f} exists, but is not a symlink."
+    fi
+  done
+}
+
+_install_dotfiles $@
