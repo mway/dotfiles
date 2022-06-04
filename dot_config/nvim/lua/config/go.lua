c51bc6db1091226d1e5beb591c35caef7c1bc06a
Matt Way <mway@uber.com>
2022-06-04 13:59:20 +0000

    Update dot_config/nvim/lua/config/go.lua

--- dot_config/nvim/lua/config/go.lua
+++ dot_config/nvim/lua/config/go.lua
@@ -2,13 +2,28 @@ vim.g.go_fmt_command = 'gopls'
 vim.g.go_imports_autosave = true
 vim.g.go_imports_mode = 'gopls'
 vim.g.go_metalinter_command = 'gopls'
+vim.g.go_def_mapping_enabled = 0
+vim.g.go_code_completion_enabled = 0
+vim.g.go_doc_keywordprg_enabled = 0
+vim.g.go_metalinter_autosave_enabled = {}
+vim.g.go_gopls_enabled = 1
+vim.g.go_term_enabled = 1
+vim.g.go_term_reuse = 1
+vim.g.go_term_mode = "split"
+vim.g.go_template_file = vim.env.HOME .. "/.config/vim-go/main.go"
+vim.g.go_template_test_file = vim.env.HOME .. "/.config/vim-go/test.go"
+
+if vim.env.VIM_GO_BIN_PATH then
+    vim.g.go_bin_path = vim.env.VIM_GO_BIN_PATH
+end
 
 local goFormatAndImports = function(wait_ms)
-    print("goFormatAndImports")
-
+    --[[
 	vim.lsp.buf.format({
 	    timeout_ms = wait_ms,
     })
+    ]]--
+    vim.lsp.buf.formatting_sync(nil, wait_ms)
 
 	local params = vim.lsp.util.make_range_params()
 	params.context = {only = {"source.organizeImports"}}
