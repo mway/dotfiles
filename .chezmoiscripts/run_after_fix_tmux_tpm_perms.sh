802b65550d1ace80f41bfcf5ea84740148c66a2c
Matt Way <mway@uber.com>
2022-06-11 13:33:36 -0400

    Update .chezmoiscripts/run_after_fix_tmux_tpm_perms.sh Update dot_tmux.conf

--- .chezmoiscripts/run_after_fix_tmux_tpm_perms.sh
+++ .chezmoiscripts/run_after_fix_tmux_tpm_perms.sh
@@ -3,5 +3,7 @@
 chmod ug+x $HOME/.tmux/plugins/tpm/tpm
 chmod ug+x $HOME/.tmux/plugins/tpm/bin/*
 chmod ug+x $HOME/.tmux/plugins/tpm/bindings/*
-chmod ug+x $HOME/.tmux/plugins/tpm/scripts/**/*
-chmod ug+x $HOME/.tmux/plugins/tpm/tests/**/*
+chmod ug+x $HOME/.tmux/plugins/tpm/scripts/*
+chmod ug+x $HOME/.tmux/plugins/tpm/scripts/*/*
+chmod ug+x $HOME/.tmux/plugins/tpm/tests/*
+chmod ug+x $HOME/.tmux/plugins/tpm/tests/*/*
