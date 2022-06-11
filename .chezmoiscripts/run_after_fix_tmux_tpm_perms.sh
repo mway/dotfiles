cc96f5330b018f96e773a114713dda4bb3565e12
Matt Way <mway@uber.com>
2022-06-11 13:09:41 -0400

    Add script to fix tpm script permissions

---
+++ .chezmoiscripts/run_after_fix_tmux_tpm_perms.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+chmod ug+x $HOME/.tmux/plugins/tpm/tpm
+chmod ug+x $HOME/.tmux/plugins/tpm/bin/*
+chmod ug+x $HOME/.tmux/plugins/tpm/bindings/*
+chmod ug+x $HOME/.tmux/plugins/tpm/scripts/**/*
+chmod ug+x $HOME/.tmux/plugins/tpm/tests/**/*
