31da24790f3421d03f6d54a2b7c0bb4e6c69ef4e
Matt Way <mway@uber.com>
2022-07-16 16:53:24 -0400

    Move tmux plugin submodules to .chezmoiexternal

--- .chezmoiscripts/run_after_fix_tmux_tpm_perms.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-chmod ug+x $HOME/.tmux/plugins/tpm/tpm
-chmod ug+x $HOME/.tmux/plugins/tpm/bin/*
-chmod ug+x $HOME/.tmux/plugins/tpm/bindings/*
-chmod ug+x $HOME/.tmux/plugins/tpm/scripts/*
-chmod ug+x $HOME/.tmux/plugins/tpm/scripts/*/*
-chmod ug+x $HOME/.tmux/plugins/tpm/tests/*
-chmod ug+x $HOME/.tmux/plugins/tpm/tests/*/*
