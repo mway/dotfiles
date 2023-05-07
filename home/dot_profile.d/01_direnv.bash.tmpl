a76269ca68ad4158274968d0372f7ab3e8ef0581
Matt Way <mway@mway.id>
2023-05-07 16:46:02 -0400

    initial zsh support

---
+++ home/dot_profile.d/01_direnv.bash.tmpl
@@ -0,0 +1,3 @@
+if command -v direnv >/dev/null 2>&1; then
+    eval "$(direnv hook bash)"
+fi
---
+++ home/dot_profile.d/40_completion_just.zsh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+
+zstyle ':completion:*:*:just:*' script "$HOME/.config/zsh/funcs/_just"
